d0b99c39ea4c569956029b8029dfd7d52380c04f
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-18 10:16:19 +0100

    new

---
+++ JS-Fundamentals 1/script.js
@@ -0,0 +1,34 @@
+//Data Types//
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Jonas");
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year)
+
+console.log(typeof null)
+
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+var job = "programmer"
+job = "teacher"
+*/
+
