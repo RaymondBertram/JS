fc86b88319231bb51f1178c3f9a1e70940f74c57
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-18 16:11:16 +0100

    mau

--- JS-Fundamentals 1/script.js
+++ JS-Fundamentals 1/script.js
@@ -60,3 +60,6 @@ console.log(x);
 //Comparison operators 
 console.log(ageJonas > ageSarah);//>,<,>=,<=
 console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
