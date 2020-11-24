456f463ec3318249e61577904110427a19240daf
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-24 11:30:31 +0100

    new

--- JS-Fundamentals 1/script.js
+++ JS-Fundamentals 1/script.js
@@ -116,4 +116,12 @@ console.log(String(23), 23);
 
 //Type Coercion
 
+console.log('I am ' + 23 + ' years old')
+console.log('23' - '10' - 3);
+console.log('23' * '2');
+console.log('23' > '18');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n)
 
