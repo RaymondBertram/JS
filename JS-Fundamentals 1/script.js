23ef2e35dcd262c70cca4cb13341ef6afc366c3a
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-24 13:46:12 +0100

    n

--- JS-Fundamentals 1/script.js
+++ JS-Fundamentals 1/script.js
@@ -126,10 +126,17 @@ n = n - 1;
 console.log(n)
 */
 
-//5 falsy values: 0, 
+//5 falsy values:
 
 console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean('Jonas'));
 console.log(Boolean({}));
 console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all;)");
+} else {
+    console.log('You should get a job!');
+}
