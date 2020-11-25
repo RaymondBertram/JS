6f48f90aa400e5ff076146951599c160d7075ba0
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-25 16:08:33 +0100

    n

--- JS-Fundamentals 1/script.js
+++ JS-Fundamentals 1/script.js
@@ -196,7 +196,7 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
 }
 */
 
-const day = 'wednesday';
+const day = 'monday';
 
 switch (day) {
     case 'monday': //day==='monday'
@@ -221,3 +221,18 @@ switch (day) {
         console.log('Not a valid day!');
 }
 
+if (day === 'monday') {
+
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day!');
+}
