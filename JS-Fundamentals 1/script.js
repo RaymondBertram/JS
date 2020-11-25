dad989be065627bfc361304c1abe7986a428f4a5
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-25 10:09:12 +0100

    new

--- JS-Fundamentals 1/script.js
+++ JS-Fundamentals 1/script.js
@@ -173,8 +173,25 @@ if (favourite !== 23) console.log('Why not 23?');
 */
 
 const hasDriverLicense = true; // A
-const hasGoodVision = false; //B 
+const hasGoodVision = true; //B 
 
 console.log(hasDriverLicense && hasGoodVision);
 console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+
+//if (hasDriverLicense && hasGoodVision) {
+//  console.log('Sarah is able to drive!')
+//} else {
+//  console.log('Somebody else should drive...')
+//}
+
+const isTired = false; //C
+console.log(hasDriverLicense && hasGoodVision && isTired);
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...')
+}
 
