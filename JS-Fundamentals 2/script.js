0edd57cf329a68b36ab5edd15e475d0ca7fd954f
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-26 13:57:39 +0100

    new

--- JS-Fundamentals 2/script.js
+++ JS-Fundamentals 2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +8,25 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 //const interface = 'Audio'; ->reserved
 //const private = 534; ->reserved
+
+
+
+*/
+
+function logger() {
+    console.log('My name is Jonas')
+}
+
+//calling/running/invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
