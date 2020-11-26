f111b32753a3e5bbad7a6894d71bf24fc4087a28
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-26 11:16:02 +0100

    new

--- JS-Fundamentals 1/assignment.js
+++ JS-Fundamentals 1/assignment.js
@@ -88,7 +88,7 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 }
 */
 
-const bill = 40;
+const bill = 400;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip} `);
 
