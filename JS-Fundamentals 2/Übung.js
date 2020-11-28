49887db4e45af2f67864b66e9771436c1ffbe2d2
Raymond Bertran <raymond.bertram@gmx.de>
2020-11-28 12:34:56 +0100

    new

--- JS-Fundamentals 2/Übung.js
+++ JS-Fundamentals 2/Übung.js
@@ -1,7 +1,39 @@
 'use strict';
-
+/*
 const now= 2020;
 const ageRay = now -2000;
 const ageColin=now-2001;
 console.log(ageColin, ageRay); 
 
+console.log(ageColin**2, ageRay*2);
+
+const firstName= 'Raymond';
+const firstName1='Colin'; 
+const lastName='Bertram';
+console.log(firstName+" "+lastName);
+console.log(firstName1+" "+lastName);
+
+let x= 15+5; //20
+x+=10; //20+10=30
+x-=10; 
+console.log(x); 
+
+console.log(ageColin>ageRay);
+console.log(ageColin>=18);  
+
+const isFullAge = ageRay>=18;
+console.log(now-2000>now-2001);
+
+const averageAge =(ageColin+ageRay)/2;
+console.log(ageColin, ageRay, averageAge); 
+
+*/
+
+const firstName='Ray';
+const lastName ='Bertram'; 
+const job='IT-Expert';
+const birthDate='16.08.2000';
+const year='2046';
+
+const infoRay=(`I'm ${firstName} ${lastName} and i was born on the ${birthDate}. In ${year} i will be a ${job} ! `);
+console.log(infoRay); 
