d27fa73f77c44db60cdbe5425c80082f80ddf547
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-11-30 11:12:03 +0100

    new

--- JS-Fundamentals 2/script.js
+++ JS-Fundamentals 2/script.js
@@ -90,7 +90,14 @@ console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
 
 */
+const cutPieces = function (fruit) {
+    return fruit * 4;
+}
+
 function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
     return juice;
 }
+console.log(fruitProcessor(2, 3));
