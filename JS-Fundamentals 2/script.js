dc11eb0524f214e52258998ff8e0b0aa30ef4a8d
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-12-04 10:54:29 +0100

    new

--- JS-Fundamentals 2/script.js
+++ JS-Fundamentals 2/script.js
@@ -150,7 +150,19 @@ const numers = calcAge(1970, 'Bruno');
 */
 
 
+//Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
 
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 64 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
 
 
 
