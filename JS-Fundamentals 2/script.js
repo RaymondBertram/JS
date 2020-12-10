846b77f6981f253b486cd1c4652838db28334d3d
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-12-10 09:38:59 +0100

    new

--- JS-Fundamentals 2/script.js
+++ JS-Fundamentals 2/script.js
@@ -177,13 +177,15 @@ const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
 console.log(jonas);
 console.log(jonas.length);
 
+
+
 //Exercise
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
 const years = [1990, 1967, 2002, 2010, 2018];
 
-//console.log(calcAge(years));-->Not right
+//console.log(calcAge(years));-->Not right-->NaN
 
 const age1 = calcAge(years[0]);
 const age2 = calcAge(years[1]);
@@ -193,7 +195,7 @@ console.log(age1, age2, age3);
 const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
 console.log(ages);
 
-//hey
+
 
 
 
