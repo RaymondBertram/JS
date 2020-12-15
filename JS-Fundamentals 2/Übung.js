c498d6bcfe5a79d4bdff79cb2b980c25402924e9
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2020-12-15 10:01:52 +0100

    new

--- JS-Fundamentals 2/Übung.js
+++ JS-Fundamentals 2/Übung.js
@@ -396,7 +396,6 @@ const vSalad2 = vegetableProcessor(5, 2, 60);
 
 console.log(vSalad1);
 console.log(vSalad2);
-*/
 
 const calcAge = year => 2020 - year;
 
@@ -419,3 +418,42 @@ const person3 = yearsUntilRetirement(1933, 'Bertram', 'Jayden');
 console.log(person1);
 console.log(person2);
 console.log(person3);
+
+
+//Arrays
+const friends = ['Michael', 'Steven', 'Bernd', 'Dennis'];
+console.log(friends);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Ray';
+const ray = [firstName, 'Bertram', 2020 - 2000, 'student', friends];
+
+console.log(ray);
+console.log(ray.length)
+*/
+
+//Array-Exercise
+const calcAge = birthYear => 2020 - birthYear;
+
+const years = [1990, 1967, 1980, 2002, 2010, 2000];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[1]), calcAge(years[2]), calcAge(years[years.length - 1])];
+
+console.log(ages);
+
+
+
+
+
+
+
