b2b8cb9ae39ced5cbee48b6c65dd0fed2dd9dbfb
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-05 15:34:36 +0100

    new

--- JS-Fundamentals 2/script.js
+++ JS-Fundamentals 2/script.js
@@ -302,15 +302,20 @@ const jonas = {
     calcAge: function () {
         this.age = 2037 - this.birthYear;
         return this.age;
-    }
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver license.`
+    },
+
+
 };
 
 console.log(jonas.calcAge());
-console.log(jonas.calcAge());
-console.log(jonas.calcAge());
 console.log(jonas.age);
 
+//Challenge 
+//Jonas is  a 46 year old teacher and he has a/no driver's license
 
-//console.log(jonas['calcAge'](1991));
-
+console.log(jonas.getSummary());
 
