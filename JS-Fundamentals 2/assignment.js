10ce544cece0c4615e82f9996d3045384edbee1a
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-07 14:05:42 +0100

    new

--- JS-Fundamentals 2/assignment.js
+++ JS-Fundamentals 2/assignment.js
@@ -68,17 +68,18 @@ Hint: Remember that an array needs a value in each position, and that value can
 actually be the returned value of a function! So you can just call a function as array
 values (so don't store the tip values in separate variables first, but right in the new
 array) �
-*/
+
 
 //1
 const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;//Ternary Operator 
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;//Ternary Operator
 }
 
-//const calcTip1 = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; //Arrow-Function 
+//const calcTip1 = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; //Arrow-Function
 
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills, tips, totals);
+
 
