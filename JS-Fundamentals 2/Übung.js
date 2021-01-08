cd98750047e8f383837467657e539105821d6fab
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-08 15:56:05 +0100

    enw

--- JS-Fundamentals 2/Übung.js
+++ JS-Fundamentals 2/Übung.js
@@ -536,4 +536,26 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills, tips, totals);
 */
+const ray = {
+    firstName: 'Ray',
+    lastname: 'Bertram',
+    birthYear: 2000,
+    job: 'student',
+    friends: ['James', 'Raphael', 'Neal'],
+    hasDriversLicense: true,
 
+    calcAge: function () {
+        this.age = 2020 - this.birthYear;
+        return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver license.`
+
+    },
+
+};
+
+console.log(ray.calcAge());
+console.log(ray.age);
+console.log(ray.getSummary()); 
