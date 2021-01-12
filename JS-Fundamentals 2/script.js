e5a8112c98b5b6a1343b8a0fbc03ee7e8bbbf851
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-12 10:31:46 +0100

    new

--- JS-Fundamentals 2/script.js
+++ JS-Fundamentals 2/script.js
@@ -335,7 +335,7 @@ console.log('Lifting weights repitition 10');
 */
 //for loop keeps running while condition is TRUE
 for (let rep = 1; rep <= 10; rep++) {
-    console.log('Lifting weights repetition 1');
+    console.log(`Lifting weights repetition ${rep}`);
 };
 
 
