7642ca5f0743601cb4691fe19aeb3fa0189c041e
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-14 14:38:21 +0100

    new

--- JS-Fundamentals 2/assignment.js
+++ JS-Fundamentals 2/assignment.js
@@ -97,7 +97,7 @@ respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
 
 Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
 tall
-*/
+
 
 
 const mark = {
@@ -134,3 +134,40 @@ if (john.bmi > mark.bmi) {
 } else {
     console.log(`Both have the same BMI of ${john.bmi}!`);
 }
+
+
+//Coding challenge 4
+Let's improve Steven's tip calculator even more, this time using loops!
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays
+*/
+
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;//Ternary Operator
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
