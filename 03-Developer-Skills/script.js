0c8cdbc7e04008f988fa90cbb0fda9e25419c4bf
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-01-19 08:37:49 +0100

    new

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,9 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
+//Problem:
+//We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
-const calcAge = birthYear => 2037 - birthYear;
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-console.log(calcAge(1991));
-console.log('IceAge');
+// 1) Understanding the problem
+// - What is the temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute the max and min temperatures?
+// - What's a sensor error? And what to do?
+
+// 2) Breaking up into sub-problems
