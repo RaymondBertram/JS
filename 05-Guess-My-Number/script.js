9449c8e2a303adc27b7b7a73f84e1c8aefea16a2
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-03-17 10:48:04 +0100

    end

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -17,16 +17,22 @@ let score = 20;
 
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   //When there is no Input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No  Number!';
+    //document.querySelector('.message').textContent = 'No  Number!';
+    displayMessage('No number!');
 
     //When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
+    // document.querySelector('.message').textContent = 'Correct Number!';
+    displayMessage('Correct Number');
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -41,18 +47,20 @@ document.querySelector('.check').addEventListener('click', function () {
     //When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        guess > secretNumber ? 'Too high!' : 'Too low!';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? 'Too high!' : 'Too low!';
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
       score--; //score = score - 1;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      // document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
-// Code replaced because the code should be clean
+// Code replaced because the code should be clean, added Ternary Operator
 /*
     //When score is too high
   } else if (guess > secretNumber) {
@@ -95,7 +103,8 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
