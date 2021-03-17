b381a2a949e62b10a37119ded4ef7d365a29b2d0
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-03-17 08:48:35 +0100

    gameover

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -15,6 +15,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -30,6 +32,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //When score is too high
   } else if (guess > secretNumber) {
