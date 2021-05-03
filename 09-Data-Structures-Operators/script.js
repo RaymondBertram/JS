220f9ab89a88da47e1a0ff6ef4e4ceb1a2c70f7a
Raymond Bertram <raymond.bertram@lifebonus-bgm.com>
2021-05-03 15:41:28 +0200

    destructure objects

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -6,9 +6,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
 
   openingHours: {
     thu: {
@@ -24,8 +21,64 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    adress,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time} `
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  adress: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  adress: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+//Destructured Object
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//Rename objects
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Set default value
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+//Nested Objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*
+///////////////////////////////////////////////////
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
@@ -35,7 +88,7 @@ console.log(main, secondary);
 // secondary = temp;
 // console.log(main, secondary);
 
-/*
+
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -44,7 +97,7 @@ const c = arr[2];
 const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
-*/
+
 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
@@ -63,3 +116,4 @@ console.log(i, j, k);
 //Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+*/
