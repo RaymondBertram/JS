77504d749500438a7b4c97d0e3844cbd7660b527
Raymond Bertran <raymond.bertram@gmx.de>
2021-06-11 11:47:09 +0200

    update

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -41,20 +41,39 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+///////////////////////////////////
+//Maps:Iteration
 
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again'],
+]);
+console.log(question);
 
+//Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(question.get(question.get('correct') === answer));
 
-
-
-
-
-
-
-
-
-
-
+//Convert Map to array
+console.log([...question]);
+console.log([...question.entries()]); //keys and values
+console.log([...question.keys()]); //keys
+console.log([...question.values()]); //values
 /*
 //////////////////////////////////////////
 //Maps
