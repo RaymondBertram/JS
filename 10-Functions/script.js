7e01185cd38cda55eba199b430b835a3a4af341f
Raymond Bertran <raymond.bertram@gmx.de>
2021-06-30 14:02:38 +0200

    changes

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,50 @@
 'use strict';
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH1234', 1, 1999);
+createBooking('LH1234', 3 , 199);
+*/
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 23432434523,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'MR.' + passenger.name;
+
+  if (passenger.passport === 23432434523) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
