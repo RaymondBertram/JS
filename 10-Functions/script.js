e78a6199f3953673bf2944795bf768b63962be2e
Raymond Bertran <raymond.bertram@gmx.de>
2021-07-01 12:09:52 +0200

    add content

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -20,7 +20,7 @@ createBooking('LH123');
 createBooking('LH1234', 1, 1999);
 createBooking('LH1234', 3 , 199);
 */
-
+/*
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -48,3 +48,30 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+//High-Order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
+const high5 = function () {
+  console.log('Clap-High');
+};
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
